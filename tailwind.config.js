ecd169585141d16d60e7372d213cd81025bb7a11
Mostafa Niakan <mostafaniakan96@gmail.com>
2022-05-24 12:21:37 +0430

    cart

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  content: ["*/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        navbar: "#004A8E",
+        textbtn: "#007aff",
+      },
+      boxShadow: {
+        "3xl": "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+      },
+    },
+  },
+};
